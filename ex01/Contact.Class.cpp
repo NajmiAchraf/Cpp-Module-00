6175cfe5f0ef9ca1598194d85653c0e88a7ac88b
NajmiAchraf <najmi.achraf@gmail.com>
2022-12-11 12:22:05 +0100

    Changes to be committed: 	new file:   .gitignore 	new file:   ex00/Makefile 	new file:   ex00/MegaPhone 	new file:   ex00/megaphone.cpp 	new file:   ex01/Contact.Class.cpp 	new file:   ex01/Contact.Class.hpp 	new file:   ex01/Makefile 	new file:   ex01/PhoneBook 	new file:   ex01/main.cpp

---
+++ ex01/Contact.Class.cpp
@@ -0,0 +1,9 @@
+#include "Contact.Class.hpp"
+
+Contact::Contact(void) {
+	std::cout << "Constructor called" << std::endl;
+}
+
+Contact::~Contact(void) {
+	std::cout << "Destructor called" << std::endl;
+}
---
+++ ex01/Contact.Class.hpp
@@ -0,0 +1,19 @@
+#ifndef CONTACT_CLASS_HPP
+# define CONTACT_CLASS_HPP
+
+# include <iostream>
+# include <string>
+# include <iomanip>
+
+std::string truncate(std::string str, size_t width);
+void	print_str(std::string str);
+void	print_int(std::int32_t integer);
+
+//class Contact {
+//
+//public:
+//	Contact(void);
+//	~Contact(void);
+//};
+
+#endif
